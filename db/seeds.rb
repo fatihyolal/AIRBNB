6c737bafe45515fe6f50aad7f92496c0123e4c6d
juwon ayorinde jaceayo123@gmail.com <jaceayo123@gmail.com>
2023-08-29 15:20:37 +0100

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do
+  User.create(email: Faker::Internet.email,
+  name: Faker::Name.name,
+  phone_number: Faker::PhoneNumber.cell_phone_with_country_code,
+  address: Faker::Address.full_address,
+  username: Faker::Internet.username,
+  is_owner: [true, false].sample
+
+  )
+
+end
+
+
+20.times do
+  Listing.create (address:Faker::Address.with_locale("it") { Faker::Address.full_address },
+  price:Faker::Commerce.price,
+  average_rating: Faker::Number.between(from: 1, to: 10),
+  availability: [true,false].sample,
+  boat_description:
+  capacity:Faker::Number.between(from: 1, to: 10),
+  user_id
+  )
+end
+
+20.times do
+  Booking.create (address:Faker::Address.with_locale("it") { Faker::Address.full_address },
+  price:Faker::Commerce.price,
+  average_rating: Faker::Number.between(from: 1, to: 10),
+  availability: [true,false].sample,
+  boat_description:
+  capacity:Faker::Number.between(from: 1, to: 10),
+  user_id
+  )
+end
+
+
+20.times do
+  Review.create (
+  comment: 
+  average_rating: Faker::Number.between(from: 1, to: 10),
+  availability: [true,false].sample,
+  boat_description:
+  capacity:Faker::Number.between(from: 1, to: 10),
+  user_id
+  )
+end
