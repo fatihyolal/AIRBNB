e5799660eb3e2ab8ad1be5ebc0745d0138d26daa
Fatih Yolal <fatihyolal@yahoo.com>
2023-08-29 17:50:47 +0300

    listingshow

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -7,6 +7,10 @@ class ListingsController < ApplicationController
     @listing = Listing.new
   end
 
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
   def edit
     @listing = Listing.find(params:id)
   end
