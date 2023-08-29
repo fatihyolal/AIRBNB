7fe51c936de21570e219d76f2e86e951495c20ff
Fatih Yolal <fatihyolal@yahoo.com>
2023-08-29 16:45:39 +0300

    migrations corrected

--- db/migrate/20230829133237_add_name_to_listings.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToListings < ActiveRecord::Migration[7.0]
-  def change
-    add_column :listings, :name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_29_132012) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_29_133237) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_132012) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_url"
+    t.string "title"
     t.index ["user_id"], name: "index_listings_on_user_id"
   end
 
@@ -68,6 +69,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_132012) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "username"
+    t.string "phone_number"
+    t.text "address"
+    t.boolean "is_owner"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
