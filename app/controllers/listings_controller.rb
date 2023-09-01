d45cb8245ddb625d6ed89d0d2b757e33c4fff1fe
Fatih Yolal <fatihyolal@yahoo.com>
2023-09-01 12:19:27 +0300

    listing reviews and routes fixed

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -18,6 +18,7 @@ class ListingsController < ApplicationController
       lng: @listing.longitude
     }]
 
+    @reviews = @listing.reviews
   end
 
   def create
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,8 @@ class ReviewsController < ApplicationController
 
   before_action :set_listing, only: %i[new create]
   def index
-    @reviews = Review.all
+    @listing = Listing.find(params[:listing_id])
+    @reviews = @listing.reviews
   end
 
   def new
